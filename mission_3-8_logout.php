4032ddcd5f27c40f35410b806805c75de26d25b1
yusukekino <34203939+yusukekino@users.noreply.github.com>
2017-12-06 13:17:26 +0900

    Add files via upload

---
+++ mission_3-8_logout.php
@@ -0,0 +1,34 @@
+﻿<html>
+<head>
+<meta charset = "UFT-8">
+<title>3-8ログアウトページ</title>
+</head>
+<body>
+<h1> <font color = "#00ff00" size = "7" face = "ＭＳ 明朝" >掲示板３－８</font></h1>
+
+
+<?php
+//データベースに接続
+$dbname = 'データベース名';
+$host = 'ホスト名';
+$user_name = 'ユーザー名';
+$password = 'パスワード';
+
+
+$pdo = new PDO("mysql:dbname=$dbname; host=$host",'$user_name','$password');
+
+
+session_start();
+
+$_SESSION = array();
+
+session_destroy();
+
+?>
+
+<a href = "mission_3-8_loginpage.php">ログインページに戻る</a>
+</body>
+</html>
+
+
+
